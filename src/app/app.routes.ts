b578be6107ef0a2ef5bf3479f5281d1552345fa3
Dheerendra singh Yadav <dheerendrasingh.yadav@conduent.com>
2024-05-05 21:26:50 +0530

    feat: :zap: cd
    m

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Home',component:DashBoardComponent},
+    {path:'AboutUs',component:AboutUsComponent},
+    {path:'ContactUs',component:ContactusComponent}
+ ];
